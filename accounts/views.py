3a47135094d47733cbd7d0801d4a440e3bd875ad
Muhammad Yaseen <66284136+myaseenml@users.noreply.github.com>
2024-07-16 17:04:29 +0500

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Login failed. Incorrect email or password.')
+    return render(request, 'login.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account successfully created!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Signup failed. Please try again.')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
